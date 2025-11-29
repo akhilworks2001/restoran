b39732612ad5f4cd9fb954ea5e55f60a366b8261
akhilworks2001 <112404416+akhilworks2001@users.noreply.github.com>
2025-11-29 17:24:21 +0530

    Normalize line endings

--- src/section/team/Team.jsx
+++ src/section/team/Team.jsx
@@ -1,29 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./team.css";
-import 'aos/dist/aos.css';
-import AOS from 'aos';
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 import chef1 from "../../assets/team/team-1.jpg";
 import chef2 from "../../assets/team/team-2.jpg";
 import chef3 from "../../assets/team/team-3.jpg";
 import chef4 from "../../assets/team/team-4.jpg";
 
-AOS.init({ duration: 800, once: true });
-
 const teamMembers = [
-  { id: 1, img: chef1, name: "Full Name", role: "Designation", delay: "0.1s" },
-  { id: 2, img: chef2, name: "Full Name", role: "Designation", delay: "0.2s" },
-  { id: 3, img: chef3, name: "Full Name", role: "Designation", delay: "0.3s" },
-  { id: 4, img: chef4, name: "Full Name", role: "Designation", delay: "0.4s" },
+  { id: 1, img: chef1, name: "Full Name", role: "Designation", delay: 100 },
+  { id: 2, img: chef2, name: "Full Name", role: "Designation", delay: 200 },
+  { id: 3, img: chef3, name: "Full Name", role: "Designation", delay: 300 },
+  { id: 4, img: chef4, name: "Full Name", role: "Designation", delay: 400 },
 ];
 
 function Team() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,  // animation time
+      once: true,     // run animation once
+    });
+  }, []);
+
   return (
     <section className="section section-container">
       <div className="container">
 
-        <div className="text-center" data-aos="fade-up" data-aos-delay="100">
+        <div
+          className="text-center"
+          data-aos="fade-up"
+        >
           <h5 className="section-title ff-secondary text-center text-primary fw-normal">
             Team Members
           </h5>
@@ -31,12 +38,12 @@ function Team() {
         </div>
 
         <section className="row g-4">
-          {teamMembers.map((member, i) => (
+          {teamMembers.map((member) => (
             <div
               className="col-lg-3 col-md-6"
-              data-wow-delay={member.delay}
+              data-aos="fade-up"
+              data-aos-delay={member.delay}
               key={member.id}
-              data-aos="fade-up" data-aos-delay={`${i+2}50`}
             >
               <div className="team-item text-center rounded overflow-hidden">
                 <div className="rounded-circle overflow-hidden m-4">
